35462cb29b91df3bc4010b067901174eb41ac1de
Amar Kulkarni <ark8su@virginia.edu>
2024-01-24 16:45:50 -0500

    add if name main guard

--- test_vector_env.py
+++ test_vector_env.py
@@ -1,27 +1,28 @@
 import gymnasium as gym
 from crash_wrappers import CrashResetWrapper, CrashRewardWrapper
-from gym.wrappers.record_video import RecordVideo
-from gym.vector import AsyncVectorEnv, SyncVectorEnv
+from gymnasium.wrappers.record_video import RecordVideo
+from gymnasium.vector import AsyncVectorEnv, SyncVectorEnv
+
+def test_vector_env():
+    reset_config = {'spawn_configs' : ['behind_left', 'behind_right', 'behind_center', 'adjacent_left', 'adjacent_right', 'forward_left', 'forward_right', 'forward_center'],
+                    'mean_distance' : 20,
+                    'initial_speed' : 20,
+                    'mean_delta_v' : 0}
+
+    crash_config = {'ttc_x_reward' : 4,
+                    'ttc_y_reward' : 1,
+                    'crash_reward' : 400,
+                    'tolerance' : 1e-3}
 
 
-reset_config = {'spawn_configs' : ['behind_left', 'behind_right', 'behind_center', 'adjacent_left', 'adjacent_right', 'forward_left', 'forward_right', 'forward_center'],
-                'mean_distance' : 20,
-                'initial_speed' : 20,
-                'mean_delta_v' : 0}
+    # Create Normal Vector Env
+    envs = gym.make_vec('highway-v0', num_envs=4,  vectorization_mode="async", render_mode='rgb_array')
+    envs.reset()
 
-crash_config = {'ttc_x_reward' : 4,
-                'ttc_y_reward' : 1,
-                'crash_reward' : 400,
-                'tolerance' : 1e-3}
+    print(f'Observation Space: {envs.observation_space}')
+    print(f'Action Space: {envs.action_space}')
+    print(f'Single Observation Space: {envs.single_observation_space}')
+    print(f'Single Action Space: {envs.single_action_space}')
 
-
-# Create Normal Vector Env
-envs = gym.make_vec('highway-v0', num_envs=4,  vectorization_mode="async", render_mode='rgb_array')
-envs.reset()
-
-print(f'Observation Space: {envs.observation_space}')
-print(f'Action Space: {envs.action_space}')
-print(f'Single Observation Space: {envs.single_observation_space}')
-print(f'Single Action Space: {envs.single_action_space}')
-
-
+if __name__ == '__main__':
+    test_vector_env()
--- test_wrappers.py
+++ test_wrappers.py
@@ -1,38 +1,41 @@
 import gymnasium as gym
 from crash_wrappers import CrashResetWrapper, CrashRewardWrapper
 
+def test_wrappers():
+    env = gym.make('highway-v0', render_mode='rgb_array')
+    env = CrashResetWrapper(env, {'spawn_configs' : ['behind_left', 'behind_right', 'behind_center', 'adjacent_left', 'adjacent_right', 'forward_left', 'forward_right', 'forward_center'],
+                                'mean_distance' : 20,
+                                'initial_speed' : 20,
+                                'mean_delta_v' : 0})
 
-env = gym.make('highway-v0', render_mode='rgb_array')
-env = CrashResetWrapper(env, {'spawn_configs' : ['behind_left', 'behind_right', 'behind_center', 'adjacent_left', 'adjacent_right', 'forward_left', 'forward_right', 'forward_center'],
-                              'mean_distance' : 20,
-                              'initial_speed' : 20,
-                              'mean_delta_v' : 0})
+    env = CrashRewardWrapper(env, {'ttc_x_reward' : 4,
+                                    'ttc_y_reward' : 1,
+                                    'crash_reward' : 400,
+                                    'tolerance' : 1e-3})
 
-env = CrashRewardWrapper(env, {'ttc_x_reward' : 4,
-                                 'ttc_y_reward' : 1,
-                                 'crash_reward' : 400,
-                                 'tolerance' : 1e-3})
+    env_config = {
+        "observation": {
+            "type": "Kinematics",
+            "normalize": False
+        },
+        "action": {
+            "type": "DiscreteMetaAction",
+            "target_speeds": list(range(15,35))
+        },
+        "lanes_count" : 2,
+        "vehicles_count" : 1,
+        "duration" : 10,
+        "initial_lane_id" : None,
+        "policy_frequency": 1
+    }
+    env.configure(env_config)
+    env.reset()
+    for _ in range(1000):
+        env.render()
+        obs, reward, terminated, truncated, info =  env.step(env.action_space.sample()) # take a random action
+        done = terminated | truncated
+        if done:
+            env.reset()
 
-env_config = {
-    "observation": {
-        "type": "Kinematics",
-        "normalize": False
-    },
-    "action": {
-        "type": "DiscreteMetaAction",
-        "target_speeds": list(range(15,35))
-    },
-    "lanes_count" : 2,
-    "vehicles_count" : 1,
-    "duration" : 10,
-    "initial_lane_id" : None,
-    "policy_frequency": 1
-}
-env.configure(env_config)
-env.reset()
-for _ in range(1000):
-    env.render()
-    obs, reward, terminated, truncated, info =  env.step(env.action_space.sample()) # take a random action
-    done = terminated | truncated
-    if done:
-        env.reset()
+if __name__ == '__main__':
+    test_wrappers()
